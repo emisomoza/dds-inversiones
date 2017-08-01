237ed1bf95e10d29b4f81d01c9b01954f3934ee8
Pablo Martin Alunni <palunni@sofrecom.com.ar>
2017-08-01 09:51:34 -0300

    Vistas

---
+++ grails-app/controllers/ar/edu/utn/dds/ConsultasController.groovy
@@ -0,0 +1,17 @@
+package ar.edu.utn.dds
+
+class ConsultasController {
+
+    def index() {
+
+    }
+
+    def consultar() {
+        render(
+                view: "/consultas",
+                model: [
+
+                ]
+        )
+    }
+}
